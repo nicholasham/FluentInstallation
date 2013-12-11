ebf29105f24eb55db618c0864c37ebcd0242006c
Kevin Holditch <kevholditch@hotmail.com>
2013-12-11 16:14:57 +0000

    Added GetParameters<T> to InstallerContext to get a strongly typed parameters object from a hashtable.  Added RequiredAttribute to make a parameter required or optional

---
+++ src/FluentInstallation.UnitTests/Catch.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace FluentInstallation
+{
+    public static class Catch
+    {
+        public static Exception Exception(Action throws)
+        {
+            Exception result = null;
+
+            try
+            {
+                throws();
+            }
+            catch (Exception exception)
+            {
+                result = exception;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ src/FluentInstallation/RequiredParameterNotGivenException.cs
@@ -0,0 +1,11 @@
+﻿namespace FluentInstallation
+{
+    public class RequiredParameterNotGivenException : InstallationException
+    {
+        public RequiredParameterNotGivenException(string parameterName)
+            : base(string.Format("Parameter {0} is required please specify it.", parameterName))
+        {
+
+        }
+    }
+}
