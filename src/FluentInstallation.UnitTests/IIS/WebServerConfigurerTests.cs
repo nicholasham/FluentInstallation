4da0b08fc7d2be4a633122cf6c012b47ebef0ee3
Kevin Holditch <kevholditch@hotmail.com>
2013-12-12 10:59:51 +0000

    Added create website, delete application pool and delete website implementations to WebServerConfigurer

--- src/FluentInstallation.UnitTests/IIS/WebServerConfigurerTests.cs
+++ src/FluentInstallation.UnitTests/IIS/WebServerConfigurerTests.cs
@@ -47,7 +47,7 @@ namespace FluentInstallation.IIS
         {
             var sut = new WebServerConfigurer();
             
-            var expected= sut.ServerManager.ApplicationPools.Count + 1;
+            var expected = sut.ServerManager.ApplicationPools.Count + 1;
 
             sut.CreateApplicationPool((options)=>{});
 
@@ -57,6 +57,44 @@ namespace FluentInstallation.IIS
             Assert.Equal("ApplicationPool" + expected, sut.ServerManager.ApplicationPools.Last().Name);
 
         }
+
+        [Fact]
+        public void DeleteApplicationPool_DeletesApplicationPoolFromServerManager()
+        {
+            var sut = new WebServerConfigurer();
+            var applicationPool = WebAdministrationFactory.CreateApplicationPool();
+            sut.ServerManager.ApplicationPools.Add(applicationPool);
+
+            sut.DeleteApplicationPool(applicationPool.Name);
+
+            Assert.Equal(0, sut.ServerManager.ApplicationPools.Count(appPool => appPool.Name == applicationPool.Name));
+
+        }
+
+        [Fact]
+        public void CreateWebsite_CreatesWebsiteOnServerManager()
+        {
+            var sut = new WebServerConfigurer();
+
+            var expected = sut.ServerManager.Sites.Count + 1;
+
+            sut.CreateWebsite(options => { });
+
+            var actual = sut.ServerManager.Sites.Count;
+
+            Assert.Equal(expected, actual);
+        }
         
+        [Fact]
+        public void DeleteWebsite_DeletesWebSiteFromTheServerManager()
+        {
+            var sut = new WebServerConfigurer();
+            var webSite = WebAdministrationFactory.CreateWebsite();
+            sut.ServerManager.Sites.Add(webSite);
+
+            sut.DeleteWebsite(webSite.Name);
+
+            Assert.Equal(0, sut.ServerManager.Sites.Count(site => site.Name == webSite.Name));
+        }
     }
 }
--- src/FluentInstallation/IIS/WebServerConfigurer.cs
+++ src/FluentInstallation/IIS/WebServerConfigurer.cs
@@ -1,5 +1,6 @@
 using System;
 using Microsoft.Web.Administration;
+using System.Linq;
 
 namespace FluentInstallation.IIS
 {
@@ -13,7 +14,7 @@ namespace FluentInstallation.IIS
         public ServerManager ServerManager { get; private set; }
 
         public void Commit()
-        {
+        {            
             throw new NotImplementedException();
         }
 
@@ -27,21 +28,37 @@ namespace FluentInstallation.IIS
 
         public IWebServerConfigurer CreateWebsite(Action<IWebsiteConfigurer> options)
         {
-            throw new NotImplementedException();
+            var defaultSiteName = string.Format("Site{0}", ServerManager.Sites.Count + 1);
+            var uniquePath = string.Format("/{0}", Guid.NewGuid().ToString("N"));
+            var site = ServerManager.Sites.Add(defaultSiteName, uniquePath, 80);
+            options(new WebsiteConfigurer(site));
+            return this;
         }
 
         public IWebServerConfigurer DeleteApplicationPool(string name)
         {
-            throw new NotImplementedException();
+            var applicationPool = ServerManager.ApplicationPools.FirstOrDefault(appPool => appPool.Name == name);
+            if (applicationPool != null)
+            {
+                ServerManager.ApplicationPools.Remove(applicationPool);
+            }
+
+            return this;
         }
 
         public IWebServerConfigurer DeleteWebsite(string name)
         {
-            throw new NotImplementedException();
+            var webSite = ServerManager.Sites.FirstOrDefault(site => site.Name == name);
+            if (webSite != null)
+            {
+                ServerManager.Sites.Remove(webSite);
+            }
+
+            return this;
         }
 
         public IDeleteApplicationConfigurer DeleteApplication(string name)
-        {
+        {                        
             throw new NotImplementedException();
         }
 
