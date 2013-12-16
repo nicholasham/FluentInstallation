36c662870042cbee992c885865184a6d58a02310
nicholasham <nicholas.hammond@gmail.com>
2013-12-16 22:01:19 +0000

    Added uninstall cmdlet

---
+++ src/FluentInstallation.UnitTests/UninstallCommandTests.cs
@@ -0,0 +1,46 @@
+﻿using System.Management.Automation;
+using NSubstitute;
+using Xunit;
+
+namespace FluentInstallation
+{
+    public class UninstallCommandTests
+    {
+        [Fact]
+        public void SutIsCmdlet()
+        {
+            var sut = new InstallCommand(Substitute.For<IInstallerFactoryFinder>());
+
+            Assert.IsAssignableFrom<Cmdlet>(sut);
+        }
+
+        [Fact]
+        public void Invoke_CallsUninstallOnAllInstallers()
+        {
+            var factoryLoader = Substitute.For<IInstallerFactoryFinder>();
+            var installerFactory = Substitute.For<IInstallerFactory>();
+
+            var installer1 = Substitute.For<IInstaller>();
+            var installer2 = Substitute.For<IInstaller>();
+
+            factoryLoader.Find().Returns(installerFactory);
+            installerFactory.Create().Returns(new[] { installer1, installer2 });
+
+            var sut = new UninstallCommand(factoryLoader);
+
+            sut.Invoke().GetEnumerator().MoveNext();
+
+            installer1.Received().Uninstall(Arg.Is<IInstallerContext>(context => context is InstallerContext));
+            installer2.Received().Uninstall(Arg.Is<IInstallerContext>(context => context is InstallerContext));
+
+        }
+
+        [Fact]
+        public void DefaultConstructor_SetsDefaults()
+        {
+            var sut = new InstallCommand();
+            Assert.NotNull(sut.Parameters);
+        }
+
+    }
+}
---
+++ src/FluentInstallation/UninstallCommand.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.Management.Automation;
+
+namespace FluentInstallation
+{
+    /// <summary>
+    ///     Uninstalls
+    /// </summary>
+    [Cmdlet(VerbsLifecycle.Uninstall, "Fluent")]
+    public class UninstallCommand : BaseCommand
+    {
+        public UninstallCommand(IInstallerFactoryFinder finder)
+            : base(finder)
+        {
+        }
+
+        public UninstallCommand()
+        {
+        }
+
+        protected override void BeginProcessing()
+        {
+            base.BeginProcessing();
+        }
+
+        protected override void ProcessRecord()
+        {
+            base.ProcessRecord();
+
+            IInstallerFactory factory = Finder.Find();
+            IEnumerable<IInstaller> installers = factory.Create();
+
+            var context = new InstallerContext(this);
+
+            foreach (IInstaller installer in installers)
+            {
+                installer.Uninstall(context);
+            }
+        }
+    }
+}
