7087c06c9a57f74fa278e8412c161392d7d144fb
nicholasham <nicholas.hammond@gmail.com>
2013-12-21 12:39:15 +0000

    Add missing file

---
+++ src/FluentInstallation/ParametersExtensions.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Reflection;
+
+namespace FluentInstallation
+{
+    public static class ParametersCastExtensions
+    {
+        public static object GetValueWithLowerInvariantKey(this IDictionary dictionary, string keyname)
+        {
+            string key =
+                dictionary.Keys.Cast<string>().FirstOrDefault(k => k.ToLowerInvariant() == keyname.ToLowerInvariant());
+
+            if (key == null)
+                return null;
+
+            return dictionary[key];
+        }
+
+        public static T Cast<T>(this IDictionary parameters) where T : class, new()
+        {
+            var result = new T();
+
+            foreach (PropertyInfo property in typeof (T).GetProperties())
+            {
+                bool required = property.GetCustomAttributes(false).Any(a => a.GetType() == typeof (RequiredAttribute));
+
+                object keyValue = parameters.GetValueWithLowerInvariantKey(property.Name);
+
+                if (required && keyValue == null)
+                    throw new RequiredParameterNotGivenException(property.Name);
+
+                if (keyValue != null)
+                {
+                    if (property.PropertyType != typeof (string))
+                    {
+                        try
+                        {
+                            keyValue = Convert.ChangeType(keyValue, property.PropertyType);
+                        }
+                        catch (FormatException)
+                        {
+                            throw new ParameterCastException(property.Name, property.PropertyType);
+                        }
+                    }
+                    property.SetValue(result, keyValue, null);
+                }
+            }
+
+
+            return result;
+        }
+    }
+}
