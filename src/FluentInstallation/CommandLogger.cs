c82d4067c87f044effe0a58bbf184aa0ea7ad8ca
nicholasham <nicholas.hammond@gmail.com>
2014-01-19 11:32:43 +0000

    Added command logger.

---
+++ src/FluentInstallation/CommandLogger.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Management.Automation;
+
+namespace FluentInstallation
+{
+    public class CommandLogger : Logger
+    {
+        private readonly ICommand _command;
+
+        public CommandLogger(ICommand command)
+        {
+            _command = command;
+        }
+
+        public override void Debug(string message, params object[] args)
+        {
+            _command.WriteDebug(string.Format(message, args));
+        }
+
+        public override void Info(string message, params object[] args)
+        {
+            _command.WriteVerbose(string.Format(message, args));
+        }
+
+        public override void Warning(string message, params object[] args)
+        {
+            _command.WriteWarning(string.Format(message, args));
+        }
+
+        public override void Error(string message, params object[] args)
+        {
+            _command.WriteError(new ErrorRecord(new InstallationException(string.Format(message, args)), string.Empty, ErrorCategory.NotSpecified, _command));
+        }
+    }
+}
