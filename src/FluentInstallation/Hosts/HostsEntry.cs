bdfdb63ce9593d58722071c64ae2ca10149bbbdc
nicholasham <nicholas.hammond@gmail.com>
2014-02-04 09:58:53 +0000

    Added start of hosts support

---
+++ src/FluentInstallation/Hosts/HostsEntry.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace FluentInstallation.Hosts
+{
+    public class HostsEntry
+    {
+        public string Description { get; set; }
+        public string IpAddress { get; set; }
+        public string HostName { get; set; } 
+    }
+
+    public class HostsEntries : List<HostsEntry>
+    {
+        public static HostsEntries FromFile(string  path)
+        {
+            return new HostsEntries();      
+        }
+    }
+
+}
